1526bce8410f3816355dd5defaa53586a82d8af5
Gabriele De Mattia <gabriele.demattia@cgm.com>
2023-09-22 17:05:34 +0200

    Unit testing

---
+++ src/test/java/guru/springframework/spring5webapp/SpringBootJpaTestSplice.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp;
+
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repository.BookRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class SpringBootJpaTestSplice {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    public void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+        bookRepository.save(new Book("Title test", "123456", "No one"));
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+}
